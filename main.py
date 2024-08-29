4a79db9bf2b7cf57197537e7284220858ebcaa6a
Kochkareva <lisskim.1821@gmail.com>
2024-08-29 11:12:00 +0400

    lw 6

--- main.py
+++ main.py
@@ -1,98 +1,79 @@
-import numpy as np
-import concurrent.futures
 import time
+import numpy as np
+from multiprocessing import Pool
 
 
-def multiplication_rows(row, matrix_b):
-    return np.dot(row, matrix_b)
+def calculate_determinant(args):
+    matrix, i = args
+    multiplier = matrix[0][i]
+    if i % 2 != 0:
+        multiplier *= -1
+    matrix = np.delete(matrix, 0, axis=0)
+    submatrix = np.delete(matrix, i, axis=1)
+    return np.linalg.det(submatrix) * multiplier
 
 
-def parallel_matrix_multiplication(matrix_a, matrix_b, num_threads):
-    num_rows_a, num_cols_a = matrix_a.shape
-    num_rows_b, num_cols_b = matrix_b.shape
-    assert num_cols_a == num_rows_b, "Размеры матриц несовместимы"
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        start_time = time.time()
+def parallel_determinant(matrix, parallel):
+    n = matrix.shape[0]
+    if parallel:
+        pool = Pool(processes=n)
         results = []
-        for i in range(num_rows_a):
-            result = executor.submit(multiplication_rows, matrix_a[i], matrix_b)
-            results.append((i, result))
-        sorted_results = sorted(results, key=lambda x: x[0])
-        result_matrix = np.vstack(
-            [result.result() for _, result in sorted_results])
+        start_time = time.time()
+        for i in range(n):
+            results.append(pool.apply_async(calculate_determinant, args=((matrix, i),)))
+        pool.close()
+        pool.join()
+        result = np.sum([res.get() for res in results])
         end_time = time.time()
-
+    else:
+        start_time = time.time()
+        result = np.linalg.det(matrix)
+        end_time = time.time()
     execution_time = end_time - start_time
-    return result_matrix, execution_time
+    return result, execution_time
 
 
 def test(parallel):
-    a = np.array([[12, 42, 64],
-                  [38, 4, 21]])
-    b = np.array([[35, 2],
-                  [64, 41],
-                  [5, 33]])
-    if parallel:
-        result = parallel_matrix_multiplication(a, b, num_threads=2)
-    else:
-        result = parallel_matrix_multiplication(a, b, num_threads=1)
-    print("Результат умножения:")
-    print(result[0])
-    print("Время выполнения: " + str(result[1]) + " с")
+    mx = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    result = parallel_determinant(mx, parallel)
+    print(f"Определитель матрицы: {result[0]}")
+    print(f"Время выполнения: {result[1]} сек.")
 
 
-def matrix100x100(parallel):
-    a = np.random.randint(0, 100, size=(100, 100))
-    b = np.random.randint(0, 100, size=(100, 100))
-    if parallel:
-        result = parallel_matrix_multiplication(a, b, num_threads=100)
-    else:
-        result = parallel_matrix_multiplication(a, b, num_threads=1)
-    print("Результат умножения:")
-    print(result[0])
-    print("Время выполнения: " + str(result[1]) + " с")
+def matrix10x10(parallel):
+    mx = np.random.randint(0, 100, size=(10, 10))
+    result = parallel_determinant(mx, parallel)
+    print(f"Определитель матрицы: {result[0]}")
+    print(f"Время выполнения: {result[1]} сек.")
 
 
-def matrix300x300(parallel):
-    a = np.random.randint(0, 100, size=(300, 300))
-    b = np.random.randint(0, 100, size=(300, 300))
-    if parallel:
-        result = parallel_matrix_multiplication(a, b, num_threads=300)
-    else:
-        result = parallel_matrix_multiplication(a, b, num_threads=1)
-    print("Результат умножения:")
-    print(result[0])
-    print("Время выполнения: " + str(result[1]) + " с")
+def matrix25x25(parallel):
+    mx = np.random.randint(0, 100, size=(25, 25))
+    result = parallel_determinant(mx, parallel)
+    print(f"Определитель матрицы: {result[0]}")
+    print(f"Время выполнения: {result[1]} сек.")
 
 
-def matrix500x500(parallel):
-    a = np.random.randint(0, 100, size=(500, 500))
-    b = np.random.randint(0, 100, size=(500, 500))
-    if parallel:
-        result = parallel_matrix_multiplication(a, b, num_threads=500)
-    else:
-        result = parallel_matrix_multiplication(a, b, num_threads=1)
-    print("Результат умножения:")
-    print(result[0])
-    print("Время выполнения: " + str(result[1]) + " с")
+def matrix50x50(parallel):
+    mx = np.random.randint(0, 100, size=(50, 50))
+    result = parallel_determinant(mx, parallel)
+    print(f"Определитель матрицы: {result[0]}")
+    print(f"Время выполнения: {result[1]} сек.")
 
 
 if __name__ == '__main__':
-    # test(parallel=True)
-    # print("Матрица 100x100:")
-    # print("Результат умножения параллельно:")
-    # matrix100x100(parallel=True)
-    # print("Результат умножения обычным способом:")
-    # matrix100x100(parallel=False)
-    # print("Матрица 300x300:")
-    # print("Результат умножения параллельно:")
-    # matrix300x300(parallel=True)
-    # print("Результат умножения обычным способом:")
-    # matrix300x300(parallel=False)
-    print("Матрица 500x500:")
-    print("Результат умножения параллельно:")
-    matrix500x500(parallel=True)
-    print("Результат умножения обычным способом:")
-    matrix500x500(parallel=False)
-
+    # print("Матрица 10x10:")
+    # print("Результат нахождения детерминанта параллельно:")
+    # matrix10x10(parallel=True)
+    # print("Результат нахождения детерминанта обычным способом:")
+    # matrix10x10(parallel=False)
+    # print("Матрица 25x25:")
+    # print("Результат нахождения детерминанта параллельно:")
+    # matrix25x25(parallel=True)
+    # print("Результат нахождения детерминанта обычным способом:")
+    # matrix25x25(parallel=False)
+    print("Матрица 50x50:")
+    print("Результат нахождения детерминанта параллельно:")
+    matrix50x50(parallel=True)
+    print("Результат нахождения детерминанта обычным способом:")
+    matrix50x50(parallel=False)
